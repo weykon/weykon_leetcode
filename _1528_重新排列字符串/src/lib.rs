a493d5900b9ce23245ccd260daa2ddc863497d51
weykon <635883092@qq.com>
2022-11-01 10:48:52 +0800

    13

--- _1528_重新排列字符串/src/lib.rs
+++ _1528_重新排列字符串/src/lib.rs
@@ -3,14 +3,17 @@
  *
  * [1528] 重新排列字符串
  */
-
+pub struct Solution;
 // @lc code=start
 impl Solution {
     pub fn restore_string(s: String, indices: Vec<i32>) -> String {
-        
+
         let letters: Vec<char> = s.chars().collect();
         let mut res = vec!['a'; s.len()];
         for (idx, &item) in indices.iter().enumerate() {
+            if letters[idx] == res[item as usize]{
+                continue;
+            }
             res[item as usize] = letters[idx];
         }
 
--- _169_多数元素/src/lib.rs
+++ _169_多数元素/src/lib.rs
@@ -34,15 +34,11 @@ impl Solution {
 // #![allow(unused_variables)]
 // fn main() {
 // use std::collections::HashMap;
-
 // let text = "hello world wonderful world";
-
 // let mut map = HashMap::new();
-
 // for word in text.split_whitespace() {
 //     let count = map.entry(word).or_insert(0);
 //     *count += 1;
 // }
-
 // println!("{:?}", map);
 // }
