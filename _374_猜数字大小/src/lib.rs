37fa0277a7f642d203fa955be1951397e6eeddfa
weykon <635883092@qq.com>
2023-10-26 14:50:43 +0800

    update

---
+++ _374_猜数字大小/src/lib.rs
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode.cn id=374 lang=rust
+ *
+ * [374] 猜数字大小
+ */
+
+// @lc code=start
+/**
+ * Forward declaration of guess API.
+ * @param  num   your guess
+ * @return 	     -1 if num is higher than the picked number
+ *			      1 if num is lower than the picked number
+ *               otherwise return 0
+ * unsafe fn guess(num: i32) -> i32 {}
+ */
+struct Solution;
+
+impl Solution {
+    unsafe fn guessNumber(n: i32) -> i32 {
+        let (a, b) = (0, n);
+         let mid = a + 
+ }
+}
+// @lc code=end
